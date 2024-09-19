963da988ea2b55c5d136804a7c35157d9489f59b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-09-19 12:43:28 +0600

    project completed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,7 @@
 function continueGame() {
     // Step-1: Generate a random alphabet
     const alphabet = getARandomAlphabet();
-    
+
     // set randomly generated alphabet to the screen
     const currentAlphabetElement = document.getElementById('current-alphabet')
     currentAlphabetElement.innerText = alphabet;
@@ -11,7 +11,66 @@ function continueGame() {
 }
 
 function play() {
+    // hide everything, show only the playgraound
     hideElementById('home-screen')
-    showElementById('play-ground')
+    hideElementById('final-score')
+    showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
 }
+
+function goHome() {
+    hideElementById('final-score');
+    showElementById('home-screen');
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('game-score', lastScore);    
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+}
+
+function handleKeyBoardKeyUpEvent(event) {
+    const playerPressed = event.key;
+
+    // stop the game if pressed 'Esc'
+    if (playerPressed === 'Escape') {
+        gameOver();
+    }
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+
+    if (playerPressed === expectedAlphabet) {
+        // update the score
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1
+        setTextElementValueById("current-score" ,updatedScore)
+
+        // start a new round
+        removeBackgroundColorById(playerPressed);
+        continueGame();
+    } else {
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;        
+        setTextElementValueById("current-life", updatedLife);
+
+        if (updatedLife === 0) {
+            gameOver();
+        }
+    }
+}
+
+document.addEventListener('keyup', handleKeyBoardKeyUpEvent)
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,10 +10,39 @@ function showElementById(elementId) {
 
 function getARandomAlphabet() {
     const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    return alphabets[Math.round(Math.random() * 26)];
+    return alphabets[Math.floor(Math.random() * 26)];
 }
 
 function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
+    if (!element) {
+        console.error(`Element with ID ${elementId} not found.`);
+        return;
+    }
     element.classList.add('bg-[#FFA500]');
+}
+
+
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#FFA500]');
+}
+
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value
+}
+
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
